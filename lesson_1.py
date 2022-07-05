f69491f7e130a4bedc84d98c717ffa06e2aa0ff4
Valeriy Svetkin <svvaliv@gmail.com>
2022-07-05 14:48:53 +0500

    baltika

--- lesson_1.py
+++ lesson_1.py
@@ -5,11 +5,4 @@ def One_func(obj: str) -> str:
 stroka = 'На вход идет эта строка'
 print(One_func(stroka))
 
-print('Baltika 9 is a nice beer!')
-print('Baltika 9 is a nice beer!')
-print('Baltika 9 is a nice beer!')
-print('Baltika 9 is a nice beer!')
-print('Baltika 9 is a nice beer!')
-print('Baltika 9 is a nice beer!')
-print('Baltika 9 is a nice beer!')
-print('Baltika 9 is a nice beer!')
+print("Baltika 9 is a great beer!")
