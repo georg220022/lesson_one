c7c1cf81b1aa18e1679783e40f3298d721d12a6d
Valeriy Svetkin <svvaliv@gmail.com>
2022-07-05 15:24:40 +0500

    nekotorie izmenenia

--- lesson_1.py
+++ lesson_1.py
@@ -1,11 +1,21 @@
 def One_func(obj: str) -> str:
-    obj = obj[::-1] #Переворачиваем строку
-    return obj #Возвращаем перевернутую сьроку
+    obj = obj[::-1]  # Переворачиваем строку
+    return obj  # Возвращаем перевернутую сьроку
+
 
 stroka = 'На вход идет эта строка'
 print(One_func(stroka))
 
 print("Baltika 9 is a great beer!")
 print("Baltika 9 is a great beer!")
-#гера взял ветку валеры и сделал изменения!
+# гера взял ветку валеры и сделал изменения!
 print('а вот и изменение!')
+
+
+def valeron(baltika: str):
+    return One_func(baltika)
+
+
+baltika = "Baltika 9 is a great beer!"
+
+print(valeron(baltika))
