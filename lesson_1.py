755bf8173996afb323ce9c19f82dff570d0636d2
georg220022 <Bhkkcfgb@yandex.ru>
2022-07-05 13:24:55 +0300

    моя часть кода

--- lesson_1.py
+++ lesson_1.py
@@ -1,7 +1,3 @@
-#гера переключился на свою ветку 'гера'
-def One_func(obj: str) -> str:
-    obj = obj[::-1] #Переворачиваем строку
-    return obj #Возвращаем перевернутую сьроку
-print('изменение в ветке моей')
-stroka = 'На вход идет эта строка'
-print(One_func(stroka))
+def My_func() -> str:
+    obj = 'Командный проект'
+    return obj
